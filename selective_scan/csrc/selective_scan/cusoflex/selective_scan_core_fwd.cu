63fd5ce5986012438704feaa23eb0488c9f45c8d
Bingo0606 <c4fpvyn2s8@privaterelay.appleid.com>
2026-07-01 22:26:38 +0800

    Update selective_scan_core_fwd.cu

--- selective_scan/csrc/selective_scan/cusoflex/selective_scan_core_fwd.cu
+++ selective_scan/csrc/selective_scan/cusoflex/selective_scan_core_fwd.cu
@@ -1,6 +1,4 @@
-/******************************************************************************
- * Copyright (c) 2023, Tri Dao.
- ******************************************************************************/
+
 #include "selective_scan_fwd_kernel_oflex.cuh"
 
 template void selective_scan_fwd_cuda<1, float, float, float>(SSMParamsBase &params, cudaStream_t stream);
